7490e318c51c7eb434b8eda93bbe0a4c85b4dd84
VA <v2517v@gmail.com>
2022-08-27 22:51:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -26,11 +26,11 @@ const App = () => {
                     {/*боковое меню:*/}
                     {activeMenu ? (
                         <div className={"w-72 fixed sidebar dark:bg-seccondary-dark-bg bg-white"}>
-                            Sidebar
+                            <Sidebar />
                         </div>
                     ) : (
                         <div className={"w-0 dark:bg-seccondary-dark-bg"}>
-                            Sidebar w-0
+                            <Sidebar />
                         </div>
                     )}
                     <div
@@ -41,34 +41,38 @@ const App = () => {
                         }
                     >
                         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-                            Navbar
+                            <Navbar />
                         </div>
                     </div>
                     <div>
+                        {/*{themeSettings && (<ThemeSettings />)}*/}
+
                         <Routes>
                             {/* dashboard  */}
-                            <Route path="/" element="Ecommerce"/>
-                            <Route path="/ecommerce" element="Ecommerce"/>
+                            <Route path="/" element={(<Ecommerce />)} />
+                            <Route path="/ecommerce" element={(<Ecommerce />)} />
+
                             {/* pages  */}
-                            <Route path="/orders" element="Ecommerce"/>
-                            <Route path="/employees" element="Ecommerce"/>
-                            <Route path="/customers" element="Ecommerce"/>
+                            <Route path="/orders" element={<Orders />} />
+                            <Route path="/employees" element={<Employees />} />
+                            <Route path="/customers" element={<Customers />} />
 
                             {/* apps  */}
-                            <Route path="/kanban" element="Ecommerce"/>
-                            <Route path="/editor" element="Ecommerce"/>
-                            <Route path="/calendar" element="Ecommerce"/>
-                            <Route path="/color-picker" element="Ecommerce"/>
+                            <Route path="/kanban" element={<Kanban />} />
+                            <Route path="/editor" element={<Editor />} />
+                            <Route path="/calendar" element={<Calendar />} />
+                            <Route path="/color-picker" element={<ColorPicker />} />
 
                             {/* charts  */}
-                            <Route path="/line" element="Ecommerce"/>
-                            <Route path="/area" element="Ecommerce"/>
-                            <Route path="/bar" element="Ecommerce"/>
-                            <Route path="/pie" element="Ecommerce"/>
-                            <Route path="/financial" element="Ecommerce"/>
-                            <Route path="/color-mapping" element="Ecommerce"/>
-                            <Route path="/pyramid" element="Ecommerce"/>
-                            <Route path="/stacked" element="Ecommerce"/>
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/area" element={<Area />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/financial" element={<Financial />} />
+                            <Route path="/color-mapping" element={<ColorMapping />} />
+                            <Route path="/pyramid" element={<Pyramid />} />
+                            <Route path="/stacked" element={<Stacked />} />
+
                         </Routes>
                     </div>
                 </div>
