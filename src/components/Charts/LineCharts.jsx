6998989bc2e95ec85c8daffd3a6cafc01959f01a
VA <v2517v@gmail.com>
2022-08-27 22:48:28 +0700

    update

--- src/components/Charts/LineCharts.jsx
+++ src/components/Charts/LineCharts.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const lineCharts = () => {
+const LineCharts = () => {
     return (
         <div>lineCharts</div>
     )
 }
-export default lineCharts
+export default LineCharts
